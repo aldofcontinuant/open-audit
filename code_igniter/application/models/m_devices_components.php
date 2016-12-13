98cc346713286276fb5e24ce1ea03990f001ad84
Mark Unwin <marku@opmantek.com>
2016-12-13 11:15:59 +1000

    Fixes for deviuces_componenets when matching attributes (used on the system_add page).

--- code_igniter/application/models/m_devices_components.php
+++ code_igniter/application/models/m_devices_components.php
@@ -583,7 +583,7 @@ class M_devices_components extends MY_Model
                 // loop through our match_columns array
                 for ($i = 0; $i < count($match_columns); $i++) {
                     // and test if the variables match
-                    if ((string)$input_item->$match_columns[$i] == (string)$output_item->$match_columns[$i]) {
+                    if ((string)$input_item->{$match_columns[$i]} === (string)$output_item->{$match_columns[$i]}) {
                         // they match so increment the count
                         $match_count ++;
                     }
@@ -614,7 +614,7 @@ class M_devices_components extends MY_Model
                 // check for a match against the columns in $match_columns
                 $match_count = 0;
                 for ($i = 0; $i < count($match_columns); $i++) {
-                    if ((string)$input_item->$match_columns[$i] == (string)$db_item->$match_columns[$i] and $db_item->updated != 'y') {
+                    if ((string)$input_item->{$match_columns[$i]} === (string)$db_item->{$match_columns[$i]} and $db_item->updated != 'y') {
                         $match_count ++;
                     }
                 }
